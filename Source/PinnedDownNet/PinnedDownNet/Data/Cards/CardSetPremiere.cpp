bbfa41e438b3091477d157baf2ed70097227faa0
Nick Prühs <dev@npruehs.de>
2014-08-25 22:45:35 +0200

    ADDED: Data - First enemy card.

--- Source/PinnedDownNet/PinnedDownNet/Data/Cards/CardSetPremiere.cpp
+++ Source/PinnedDownNet/PinnedDownNet/Data/Cards/CardSetPremiere.cpp
@@ -22,6 +22,15 @@ Entity CardSetPremiere::CreateCard(int cardIndex)
 		this->cardFactory->SetFlagship(card);
 		this->cardFactory->SetPower(card, 8);
 		this->cardFactory->SetThreat(card, 4);
+		break;
+
+	case 73:
+		// Enemy - Assault Frigate.
+		card = this->cardFactory->CreateStarship(this->GetSetIndex(), 73);
+		this->cardFactory->SetAffiliation(card, Affiliation::Enemy);
+		this->cardFactory->SetPower(card, 8);
+		this->cardFactory->SetThreat(card, 2);
+		break;
 	}
 
 	this->cardFactory->FinishCard(card);
