4456588b6eacbe25fd5be6a53faed4008d37e4f5
Nick Prühs <dev@npruehs.de>
2014-09-14 10:27:25 +0200

    ADDED: Event - Error message.

---
+++ Source/PinnedDownNet/PinnedDownNet/Events/ErrorMessageEvent.cpp
@@ -0,0 +1,15 @@
+#include "ErrorMessageEvent.h"
+
+using namespace PinnedDownNet::Events;
+
+const HashedString ErrorMessageEvent::ErrorMessageEventType = HashedString("ErrorMessage");
+
+void ErrorMessageEvent::Serialize(std::ostrstream& out)
+{
+	out << this->errorMessage << " ";
+}
+
+void ErrorMessageEvent::Deserialize(std::istrstream& in)
+{
+	in >> this->errorMessage;
+}
---
+++ Source/PinnedDownNet/PinnedDownNet/Events/ErrorMessageEvent.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+
+#include "Event.h"
+
+using namespace PinnedDownCore;
+
+namespace PinnedDownNet
+{
+	namespace Events
+	{
+		struct ErrorMessageEvent : public PinnedDownCore::Event
+		{
+			static const HashedString ErrorMessageEventType;
+
+			const HashedString & GetEventType() const
+			{
+				return ErrorMessageEventType;
+			}
+
+			const NetRole GetNetRole() const
+			{
+				return NetRole::Client;
+			}
+
+			std::string errorMessage;
+
+			explicit ErrorMessageEvent() : ErrorMessageEvent("")
+			{
+			}
+
+			explicit ErrorMessageEvent(std::string errorMessage)
+			{
+				this->errorMessage = errorMessage;
+			}
+
+			void Serialize(std::ostrstream& out);
+			void Deserialize(std::istrstream& in);
+		};
+	}
+}
